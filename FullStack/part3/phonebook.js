9fd1484d88d9a1c5f2fb83b27b4a6434a520658e
chhimi12 <chhimi101@gmail.com>
2025-11-17 21:17:42 -0700

    connected to DB

--- FullStack/part3/phonebook.js
+++ FullStack/part3/phonebook.js
@@ -1,16 +1,19 @@
+
 const http = require('http')
 const express = require('express')
 let morgan = require('morgan')
 const cors = require('cors')
 
+const phonebook = require('./models/db_phonebook')
+const { error } = require('console')
 
-// nto working
 
 const app = express()
+app.use(express.json()) // parses request received from JSON to java script object and attaches to request body
+
 app.use(express.static('dist'))
 app.use(cors()) // allows request from all origins
 
-app.use(express.json()) // parses request received from JSON to java script object and attaches to request body
 
 morgan.token('body', (req) => {
   console.log("call reached here, middleware activated")
@@ -18,95 +21,96 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] :body - :response-time ms'))
 
-// app.use('tiny')
-
-let phoneBook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    
-]
-
-
 app.get('/api/persons',(request, response) => {
-    response.json(phoneBook)
+    phonebook.find({}).then(result =>{
+      console.log(result)
+      response.json(result)
+    })
 }) 
 
-app.get('/api/persons/5',(request, response) => {
-
-  if(phoneBook[5]) {
-      response.json(phoneBook[5])
-  }
-  else {
-    response.status(404).end() // set the status and do not send any data
-  }
-
-})
-
-
-app.delete('/api/persons/:id',(request, response) => {
+app.get('/api/persons/:id',(request, response, next) => {
   const id = request.params.id
-  if(phoneBook[id]) {
-    phoneBook = phoneBook.filter(note => note.id != id)   
-    response.json(phoneBook) 
-  }
 
-  else {
-    response.status(404).end()
-  }
+  phonebook.find({_id:id}).then(result => {
+    response.json(result)
+    response.status(204).end()
+  }).catch(error => next(error))
 
 })
 
-app.post('/api/persons',(request, response) => {
+app.put('/api/persons/:id',(request, response, next) => {
+  const id = request.params.id
+  const {name, number} = request.body
 
+  phonebook.findById(id).then(result => {
+    if(!result) {
+      return response.status(404).end()
+    }
+    result.name = name
+    result.number = number
 
-  const new_phone = request.body
-  if (phoneBook.find(item => item.number === new_phone.number ))
-  {
-    response.statusMessage = "Phone number already exists"
-    response.status(404).end()
-  }
-
-  if(new_phone.name && new_phone.number) {
-  const randomId = Math.floor(Math.random()*100)
-  new_phone.id = randomId
-
-  phoneBook.push(new_phone)
-  }
-
-  else {
-    response.status(404).end()
-  }
-
- console.log(new_phone)
+    return result.save().then((updatedPhone) => {
+      response.json(updatedPhone)
+    })
+  }).catch(error => next(error))
 
 })
+
+app.delete('/api/persons/:id',(request, response, next) => {
+  console.log("call reached to delete")
+  const id = request.params.id
+
+  phonebook.findByIdAndDelete(id).then(result => {
+    response.status(204).end()
+  }).catch(error => next(error))
+
+})
+
+
+
+
+app.post('/api/persons',(request, response, next) => {
+ 
+ 
+  console.log("call reached to add the person")
+  const new_phone = new phonebook({
+    name: request.body.name,
+    number: request.body.number
+  })
+
+  new_phone.save().then(savedContact => {
+    response.json(savedContact)
+  }).catch(error => next(error))
+  
+
+})
+
 
 app.get('/info', (request, response) => {
     const now = Date()
     
-    response.send(`<b><p>The phonebook has ${phoneBook.length} entries.</p>${now}</b>`)
+    response.send(`<b><p>The phonebook has ${phonebook.length} entries.</p>${now}</b>`)
 
 })
 
-const PORT = process.env.PORT || 3001
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
      console.log(`Server running on port ${PORT}`)
 })
